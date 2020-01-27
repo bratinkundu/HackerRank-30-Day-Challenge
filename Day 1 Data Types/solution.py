c4bef6aa8612180af0e9ebe71b0a75b6dafd0f75
Bratin Kundu <36078208+bratinkundu@users.noreply.github.com>
2020-01-27 23:19:36 +0530

    Add files via upload

---
+++ Day 1 Data Types/solution.py
@@ -0,0 +1,13 @@
+# Declare second integer, double, and String variables.
+a = None
+b = None
+c = None
+# Read and save an integer, double, and String to your variables.
+a = int(input())
+b = float(input())
+c = str(input())
+
+# Print the sum of both integer variables on a new line.
+print(i+a)
+print(d+b)
+print(s+c)
